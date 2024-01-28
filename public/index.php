ecd01248d96b8add7ad1c7bff42ea406fca66a84
Momaa73 <107126681+Momaa73@users.noreply.github.com>
2024-01-28 14:09:36 +0200

    upload: index

--- public/index.php
+++ public/index.php
@@ -48,7 +48,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-
+/////////////////////////////////////////////////////// mine
+echo "MORANNNN"
+/////////////////////////////////////////////////////// mine
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
